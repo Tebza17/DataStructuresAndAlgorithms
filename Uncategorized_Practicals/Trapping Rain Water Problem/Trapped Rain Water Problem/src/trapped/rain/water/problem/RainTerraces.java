ea306a91f0f7802796810258469ea22be36931f9
tebza17 <tebza17.tm@gmail.com>
2019-09-06 08:16:54 +0200

    Solving rain water with dynamic programming

--- Uncategorized_Practicals/Trapping Rain Water Problem/Trapped Rain Water Problem/src/trapped/rain/water/problem/RainTerraces.java
+++ Uncategorized_Practicals/Trapping Rain Water Problem/Trapped Rain Water Problem/src/trapped/rain/water/problem/RainTerraces.java
@@ -6,9 +6,25 @@
 package trapped.rain.water.problem;
 
 /**
- *
+ * @Description: I will use dynamic programming for solving Trapping Rain Water problem.
  * @author Vincent
  */
 public class RainTerraces {
+    public int TrappedWaterCalculator(int[] terraces){
+        int amountWater = 0;
+        int[] leftMax = new int[terraces.length], rightMax = new int[terraces.length];
+        for(int i=0; i< terraces.length; i++){
+            leftMax[i] = rightMax[i] = 0;
+        }
+        
+        return 0;
+    }
+    public int[] highestLeftLevel(int[] lMaxLevel, int[] terraces){
+        lMaxLevel[0] = terraces[0];
+        for(int terraceIndex = 1; terraceIndex < terraces.length; terraceIndex++){
+            lMaxLevel[terraceIndex] = Math.max(terraces[terraceIndex], lMaxLevel[terraceIndex - 1]);
+        }
+        return lMaxLevel;
+    }
     
 }
